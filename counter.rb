a30e0bdb452f97ade0ac1fd5e91a37799d4a9103
wordkarin <karin.kubischta@gmail.com>
2016-03-29 22:07:57 -0700

    learned about for..in loops

---
+++ counter.rb
@@ -0,0 +1,15 @@
+#initialize the variables
+
+i = 0
+array = ['red', 'blue', 'green', 'orange', 'yellow']
+
+#print the elements in the array
+for color in array
+  puts color
+end
+
+for i in 1..10
+  puts i
+end
+
+#DONE.
