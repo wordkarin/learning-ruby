e4af0940d78500db1fe2641a532d203ffd953cfd
Karin <karin@smalls.(none)>
2016-03-29 20:26:05 -0700

    I'm adding pets

---
+++ pets.rb
@@ -0,0 +1,30 @@
+#collect information about a person and their animal and store that information in variables
+puts "Tell me your name!"
+user_name = gets.chomp
+
+puts "Do you have a pet?"
+pet_bool = gets.chomp
+
+if pet_bool == "yes" then
+puts "What is your pet's name?"
+pet_name = gets.chomp
+
+puts "What kind of animal is your pet?"
+pet_kind = gets.chomp
+
+puts "How many legs does your pet have?"
+leg_count = gets.chomp
+
+puts "Is your pet tall, sleek, or stout?"
+pet_description = gets.chomp
+
+#use those variables to describe the animal in the console output
+
+puts user_name + "'s pet is named " + pet_name + ". " +
+pet_name + " is a " + pet_kind + "."
+puts pet_name + " has #{leg_count} legs and is " + pet_description + "."
+else
+	puts "I'm sorry, " + user_name + ". That's too bad."
+end
+
+#DONE!
