971de497109ac74d6ffcfa6b77583ceadffcdd71
Miles Crawford <milesc@allenai.org>
2016-05-21 12:54:42 -0700

    downcase everything for lookup

--- rewriter/words.rb
+++ rewriter/words.rb
@@ -1,20 +1,19 @@
 class Words 
 
-
     def initialize(wordsFile)
         @partsByWord = {}
         File.open(wordsFile, "r") do |f|
             f.each_line do |line|
 
                 (word, part) = line.split("\t")
-                @partsByWord[word] = part
+                @partsByWord[word.downcase] = part.downcase
 
             end
         end
     end
 
     def lookupPart(word)
-        return @partsByWord[word]
+        return @partsByWord[word.downcase]
     end
 
 end
