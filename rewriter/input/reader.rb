fdc2600b1ec7afe1dc578b798f5d94889a2de381
Karin Kubischta <karinkubischta@Karins-MacBook.local>
2016-05-21 11:41:30 -0700

    reader now splits input text into an array.

---
+++ rewriter/input/reader.rb
@@ -0,0 +1,14 @@
+#this is a program that will take input, store that input as a variable, and print it back to the console
+puts "enter some text please."
+starting_text = ""
+
+while text = gets
+  starting_text = starting_text + text
+end
+
+def tokenize(text)
+  word_array = []
+  word_array = text.split
+end
+,
+puts tokenize(starting_text)
